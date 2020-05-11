b29e0c4fec4337a83dcaad7d579bdbe69907139c
Ruithlzz09 <bisht09sachin@gmail.com>
2020-05-11 12:13:48 +0530

    Refactor Design and disallow Empty Task

--- src/app.py
+++ src/app.py
@@ -15,54 +15,54 @@ class Todo(db.Model) :
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
+    def __repr__(self) :
         return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
+def index() :
+    if request.method == 'POST' and request.form['content'] != '':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
-        try:
+        try :
             db.session.add(new_task)
             db.session.commit()
             # print('POST REQ')
             return redirect('/')
-        except Exception as err:
+        except Exception as err :
             return f'There was an issue adding your task. {err}'
 
-    else:
+    else :
         # print('GET REQ')
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 
 @app.route('/delete/<int:uid>')
-def delete(uid):
+def delete(uid) :
     task_to_delete = Todo.query.get_or_404(uid)
-    try:
+    try :
         db.session.delete(task_to_delete)
         db.session.commit()
         # print('POST REQ')
         return redirect('/')
-    except Exception as err:
+    except Exception as err :
         return f'There was an issue deleting your task. {err}'
 
 
 @app.route('/update/<int:uid>', methods=['POST', 'GET'])
-def update(uid):
+def update(uid) :
     task = Todo.query.get_or_404(uid)
-    if request.method == 'POST':
+    if request.method == 'POST' :
         task.content = request.form['content']
-        try:
+        try :
             db.session.commit()
             return redirect('/')
-        except Exception as err:
+        except Exception as err :
             return f'There was an issue updating your task. {err}'
     else :
         return render_template('update.html', task=task)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     app.run(debug=True)
